1a83501446c4a0058a630d68c2fbb0c8d922191d
Joonas Hiltunen <joonas.s.jh@gmail.com>
2022-03-06 22:51:41 +0200

    Fix app not uploading

--- App.js
+++ App.js
@@ -2,11 +2,46 @@ import {StatusBar} from 'expo-status-bar';
 import React from 'react';
 import {Platform, SafeAreaView, StyleSheet, View} from 'react-native';
 import List from './components/List';
+import { useForm } from 'react-hook-form';
+import {useMedia} from './hooks/ApiHooks'
+
 
 const App = () => {
+  const {uploadNewCoffee} = useMedia();
+  const { register, handleSubmit } = useForm();
+  const onSubmit = async (data) => await uploadNewCoffee(data);
+  //const onSubmit = (data) => console.log(data);
+  //{...register("weight", {valueAsNumber: true})} placeholder="Packet weight in grams" />*/
   return (
     <SafeAreaView style={styles.droidSafeArea}>
       <View style={styles.container}>
+      <form onSubmit={handleSubmit(onSubmit)}>
+
+      <input {...register("name")} placeholder="Cofee name" />
+
+      <input type="number"
+        {...register("weight", {
+          valueAsNumber: true,
+        })}
+        />
+      
+      <input {...register("price", {
+          valueAsNumber: true,
+        })} placeholder="Packet price in cents"/>
+        
+        <select {...register("roast_level", {
+          valueAsNumber: true,
+        })}>
+          <option value="">Select coffee roast level...</option>
+          <option value="1">1</option>
+          <option value="2">2</option>
+          <option value="3">3</option>
+          <option value="4">4</option>
+          <option value="5">5</option>
+        </select>
+
+        <input type="submit" />
+      </form>
         <List />
         <StatusBar style="auto" />
       </View>
